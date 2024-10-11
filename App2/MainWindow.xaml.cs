4ddf24a532e80c045083c237b6253524a69a49cc
Bryanndefo <ndefobryan2005@gmail.com>
2024-10-11 13:44:21 -0400

    Ajoute de la navigation

--- App2/MainWindow.xaml.cs
+++ App2/MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
 using Microsoft.UI.Xaml.Navigation;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -31,6 +23,76 @@ namespace App2
         private void StackPanel_ContextCanceled(UIElement sender, RoutedEventArgs args)
         {
 
+        }
+
+        private void ContentFrame_NavigationFailed(
+                        object sender, NavigationFailedEventArgs e)
+        {
+            throw new Exception("Impossible de charger la page " +
+                e.SourcePageType.FullName);
+        }
+
+        private void NavView_Loaded(object sender, RoutedEventArgs e)
+        {
+            ContentFrame.Navigated += On_Navigated;
+            // Au chargement de la page, charger la première page du menu
+            NavView.SelectedItem = NavView.MenuItems[0];
+        }
+
+        private void NavView_SelectionChanged(
+            NavigationView sender, NavigationViewSelectionChangedEventArgs args)
+        {
+            if (args.SelectedItemContainer != null)
+            {
+                Type navPageType = Type.GetType(args.SelectedItemContainer.Tag.ToString());
+                NavView_Navigate(navPageType, args.RecommendedNavigationTransitionInfo);
+            }
+        }
+
+        private void NavView_Navigate(
+            Type navPageType, NavigationTransitionInfo transitionInfo)
+        {
+            if (navPageType is not null)
+            {
+                ContentFrame.Navigate(navPageType, null, transitionInfo);
+            }
+        }
+
+        private void NavView_BackRequested(NavigationView sender,
+            NavigationViewBackRequestedEventArgs args)
+        {
+            TryGoBack();
+        }
+
+        private bool TryGoBack()
+        {
+            if (!ContentFrame.CanGoBack)
+            {
+                return false;
+            }
+            else
+            {
+                ContentFrame.GoBack();
+                return true;
+            }
+        }
+
+        private void On_Navigated(object sender, NavigationEventArgs e)
+        {
+            NavView.IsBackEnabled = ContentFrame.CanGoBack;
+
+            if (ContentFrame.SourcePageType != null)
+            {
+                // Trouver la page à charger
+                NavView.SelectedItem = NavView.MenuItems
+                            .OfType<NavigationViewItem>()
+                            .First(i => i.Tag.Equals(ContentFrame.SourcePageType.
+                                                                    FullName.ToString()));
+
+                // Afficher dans l'entête le titre de la page
+                NavView.Header = ((NavigationViewItem)NavView.SelectedItem)?.
+                                                        Content?.ToString();
+            }
         }
     }
 }
